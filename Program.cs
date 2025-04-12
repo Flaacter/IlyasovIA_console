79c8089c2ee74cc53c2d64c0ebf2295cc3bb8c8f
Flaacter <ilnar.ilyasov75@gmail.com>
2025-04-12 22:00:16 +0300

    Add * .cs-files

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IlyasovIA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }//solve task2
+}//Ильясов Ильнар Айратович Ам-1-24
